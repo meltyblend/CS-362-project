17be3ecff9a2507e6bed319ecd1d0998713e48d7
Aren <106656925+meltyblend@users.noreply.github.com>
2025-02-26 00:10:23 -0800

    This is where it begins.

---
+++ README.md
@@ -0,0 +1,9 @@
+version of node we all must be on
+v23.7.0
+
+npm version we all must be on
+10.9.2
+
+python version we all must be on
+Python 3.12.6
+
---
+++ app.py
@@ -0,0 +1,19 @@
+#this is where the backend starts and where all of the server side routing is going to be done
+
+
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='frontend/static')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    # If a static file exists for the requested path, serve it
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    # Otherwise, serve index.html for Vue Router to handle routing
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
